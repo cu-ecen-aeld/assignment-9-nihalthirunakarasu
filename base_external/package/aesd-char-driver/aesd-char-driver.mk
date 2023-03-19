dd263bc8a85b0b8bef355cd5730b3ce67f7bde78
Nihal T <nihalt.95@gmail.com>
2023-03-19 15:05:39 +0000

    Final Push

---
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -0,0 +1,27 @@
+
+##############################################################
+#
+# AESD-CHAR-DRIVER
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+#Link for A3 after changes for socket : https://github.com/cu-ecen-aeld/assignments-3-and-later-nihalthirunakarasu/commit/8c12a2efdf205c44c1408b8fb4da849ce191466e
+AESD_CHAR_DRIVER_VERSION = '7d2d2a0dafba1711c4feb9dc915d7f47f23d5ba6'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_CHAR_DRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-nihalthirunakarasu.git'
+AESD_CHAR_DRIVER_SITE_METHOD = git
+AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
+
+AESD_CHAR_DRIVER_MODULE_SUBDIRS += aesd-char-driver/
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define AESD_CHAR_DRIVER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
